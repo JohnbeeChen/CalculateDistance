2884f03c5e83026d0dfad86fcf172c68a9e0fc98
Johnbee <Johnbee13@outlook.com>
2017-07-21 19:20:15 +0800

    nothing
    Signed-off-by: Johnbee <Johnbee13@outlook.com>

--- test.m
+++ test.m
@@ -1,21 +1,10 @@
-load fitdata.mat;
-
-
-fitresult = BleachCorrection(y);
-% num_y = length(y);
-% 
-% 
-% myfunc=inline('beta(1) + beta(2)*exp(-beta(3)*x)','beta','x'); 
-% 
-% %beta=nlinfit(x,y,myfunc,[0.5 0.5 0.5 0.5]); 
-% beta=nlinfit(x,y,myfunc,[0.5 0.5 0]); 
-% 
-% %a=beta(1);k1=beta(2);m=beta(3);k2=beta(4);
-% a=beta(1);k1=beta(2);m=beta(3);
-% test_x = [1:num_y];
-% test_y = a+k1*exp(-m*x);
-% 
-% jz_trace = y-test_y;
-plot(y);
-hold on
-plot(fitresult);
+rng default
+load kmeansdata.mat
+size(X)
+idx3 = kmeans(X,3,'Distance','cityblock');
+figure 
+[silh3,h] = silhouette(X,idx3,'cityblock');
+h = gca;
+h.Children.EdgeColor = [0.8,0.8,1];
+xlabel 'Silhouette Value';
+ylabel 'Cluster';
